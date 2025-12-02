84d18c720f47a7930a2bbfb31ec481af1efdcf2f
Alvaro-bit-stack <aizquierdo1325@gmail.com>
2025-12-02 14:49:24 -0500

    fixed circ

--- translater.py
+++ translater.py
@@ -1,8 +1,8 @@
 imapping = {
-    "ADDY":0b00000000,
-    "SUBBY":0b01000000,
+    "SUBBY":0b00000000,
+    "LDUR":0b01000000,
     "STUR": 0b10000000,
-    "LDUR":0b11000000,
+    "ADDY":0b11000000,
     "X0":[0b00000000,0b00000000,0b00000000],
     "X1" :[0b00010000,0b00000100, 0b00000001],
     "X2":[0b00100000,0b00001000,0b00000010]}
@@ -23,9 +23,19 @@ def decoder(instruction):
     return machinecode
 def decoding(instructions):
     with open("image_file.txt", "w") as file:
+        file.write("v3.0 hex words addressed\n")
+        newLine = True
         for address,instruction in enumerate(instructions):
             decoded = decoder(instruction)
-            file.write(f"{hex(address)}:{hex(decoded)}\n")
+            if address % 16 == 0 and address != 0:
+                file.write(f"{decoded:02x}\n")
+                newLine = True
+            elif newLine:
+                file.write(f"{address:02x}: {decoded:02x} ")
+                newLine = False
+            else:
+                file.write(f"{decoded:02x} ")
+                
 
 instructions = []       
 
@@ -35,3 +45,4 @@ with open("program.txt","r") as file:
         instructions.append(instruction)
 
 decoding(instructions)
+print("done")
